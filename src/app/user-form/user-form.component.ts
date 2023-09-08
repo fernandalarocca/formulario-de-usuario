1af9bb3bc81f6e660b73790eda75d0e2d5383e20
Fernanda Larocca <fernandacolacovaz@hotmail.com>
2023-09-08 16:45:24 -0300

    upando o projeto

---
+++ src/app/user-form/user-form.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.css']
+})
+export class UserFormComponent implements OnInit {
+  userForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      console.log('Dados do usuário:', this.userForm.value);
+    }
+  }
+}
